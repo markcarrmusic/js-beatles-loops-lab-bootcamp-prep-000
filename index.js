cb6696fcf3ee3abced5b52898d07ade7e53cafff
markcarrmusic@gmail.com <markcarrmusic@gmail.com>
2018-04-17 17:50:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+// What instrument do the members of The Beatles play loop
+function theBeatlesPlay(musicians, instruments) {
+  var Beatles = [];
+  for (let i = 0; i < 4; i++) {
+    Beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return Beatles;
+}
+
+// John Lennon facts
+function johnLennonFacts(facts) {
+  var i = 0;
+  var yellingFacts = [] 
+  while(yellingFacts.length < facts.length) {
+   yellingFacts.push(facts[i]+'!!!');
+    i++;
+  }
+ return yellingFacts;
+}
